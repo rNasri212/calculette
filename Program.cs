fd38942a7739473938a37ebfc3af635368e1706d
Rayan Nasri <147519632+rNasri212@users.noreply.github.com>
2023-10-10 16:31:45 +0200

    commit 1.0

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,12 +16,30 @@ namespace calculette
             * 10.10.2023
             */
         {
-            int ch1; 
+
+            int ch1;
             int ch2;
+            Console.WriteLine("calculette"); 
             Console.WriteLine("premier chiffre ");
             ch1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("premier chiffre ");
+            Console.WriteLine("deuxieme chiffre ");
             ch2 = Convert.ToInt32(Console.ReadLine());
+
+            switch ((ch1)
+)
+            {
+                case 1:
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    Console.WriteLine();
+                    break;
+                default:
+                    break;
+            }
+
+
+
 
 
 
